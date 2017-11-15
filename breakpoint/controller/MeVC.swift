e94d63f6c6c5fb468ee721af0d7b5ced0b677367
Andrew <milldog8@hotmail.com>
2017-11-15 14:51:39 -0700

    MeVC, tabbar controller

---
+++ breakpoint/controller/MeVC.swift
@@ -0,0 +1,30 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Andrew on 11/15/17.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var emailLbl: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+    @IBAction func singOutBtnWasPressed(_ sender: Any) {
+        
+    }
+    
+
+}
