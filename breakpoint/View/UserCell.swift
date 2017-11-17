ebe1e7685078b857c1dc540f5a917e4d1c59eb04
Andrew <milldog8@hotmail.com>
2017-11-17 11:08:44 -0700

    UserCell

---
+++ breakpoint/View/UserCell.swift
@@ -0,0 +1,33 @@
+//
+//  UserCell.swift
+//  breakpoint
+//
+//  Created by Andrew on 11/17/17.
+//  Copyright © 2017 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    func configureCell(profileImage image: UIImage, email: String, isSelected: Bool) {
+     self.profileImage.image = image
+        self.emailLbl.text = email
+        if isSelected {
+            self.checkImage.isHidden = false
+        } else {
+            self.checkImage.isHidden = true
+        }
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- breakpoint/controller/CreateGroupsVC.swift
+++ breakpoint/controller/CreateGroupsVC.swift
@@ -20,8 +20,9 @@ class CreateGroupsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
 
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func doneBtnWasPressed(_ sender: Any) {
@@ -30,4 +31,20 @@ class CreateGroupsVC: UIViewController {
     @IBAction func closeBtnWasPressed(_ sender: Any) {
     }
     
+}
+
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else {return UITableViewCell() }
+        let profileImage = UIImage(named: "defaultProfileImage")
+        
+        cell.configureCell(profileImage: profileImage!, email: "marty@bttf.com", isSelected: true)
+        return cell
+    }
 }
